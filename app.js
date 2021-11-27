a5d082195b8e2f071175c1a9c5d052581aba4fc1
enesayyildiz1 <ayyildiz_enes66@hotmail.com>
2021-11-27 22:03:47 +0300

    ejs entegrasyonu yapıldı

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+const { application } = require('express');
 const express = require('express')
+const ejs = require('ejs')
 const app = express()
- 
+app.use(express.static('public'))
+app.set("view engine","ejs")
+const myLogger=(req,res,next)=>{
+    console.log("Middleware 1");
+    next();
+}
+app.use(myLogger);
 app.get('/', function (req, res) {
-    const blog = { 
-        id: 1,
-        title: "Blog title", 
-        description: "Blog description" }
-  res.send(blog)
+    res.render("index")
+})
+app.get('/about', function (req, res) {
+  res.render("about")
+})
+app.get('/post', function (req, res) {
+  res.render("post")
+})
+app.get('/add', function (req, res) {
+  res.render("add_post")
 })
  
 app.listen(3000)
